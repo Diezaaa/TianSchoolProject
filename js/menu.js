a0f4209c7ced3ba4348aaab73a8ef443cc324c60
Daniel Gertsykov <kotvesopogah@gmail.com>
2024-09-12 09:06:22 +0300

    menu fix

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 let btn = document.getElementById("menu");
 let menu = document.getElementById("menu_slider");
-let openStyles = 'backdrop-filter: blur(10px);background-color: rgba(215, 220, 227, 0.48); height: calc(100vh - 88.25px);position:absolute;;width: 90%; max-width: 800px; margin-right:auto;margin-top:15px;margin-left:auto;    border-radius: 20px; display: flex;    transform: translateX(-50%);    left: 50%;';
+let openStyles = 'backdrop-filter: blur(10px);background-color: rgba(215, 220, 227, 0.48); height: 100vh;position:absolute;;width: 90%; max-width: 800px; margin-right:auto;margin-top:15px;margin-left:auto;    border-radius: 20px; display: flex;    transform: translateX(-50%);    left: 50%;';
 
 btn.addEventListener("click", function(){
     if (menu.classList.contains("closed")){
